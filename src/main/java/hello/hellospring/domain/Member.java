ac325c641e031af5dd82a9ab2180b1347dadf525
khb6997@gmail.com <khb6997@gmail.com>
2024-04-30 23:19:34 +0900

    데이터베이스 설계에 따른 Entity 클래스 생성

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,37 @@
+package hello.hellospring.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString(of = {"id", "email", "password", "nickname"})
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    @Builder
+    public Member(Long id, String email, String password, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    @OneToMany(mappedBy = "member")
+    private List<Posts> posts = new ArrayList<>();
+
+    public void update(String nickname) {
+        this.nickname = nickname;
+    }
+}
