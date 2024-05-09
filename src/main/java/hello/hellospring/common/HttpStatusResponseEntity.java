1e4332b8805b93ef659fd519dddf99713d749bb0
khb6997@gmail.com <khb6997@gmail.com>
2024-05-09 22:20:46 +0900

    비인가 시 응답 추가

--- src/main/java/hello/hellospring/common/HttpStatusResponseEntity.java
+++ src/main/java/hello/hellospring/common/HttpStatusResponseEntity.java
@@ -6,5 +6,6 @@ import org.springframework.http.ResponseEntity;
 public class HttpStatusResponseEntity {
     public static final ResponseEntity<HttpStatus> RESPONSE_OK = ResponseEntity.status(HttpStatus.OK).build();
     public static final ResponseEntity<HttpStatus> RESPONSE_NOT_FOUND = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    public static final ResponseEntity<HttpStatus> RESPONSE_UNAUTHORIZED = ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public static final ResponseEntity<HttpStatus> RESPONSE_UNAUTHENICATED = ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public static final ResponseEntity<HttpStatus> RESPONSE_UNAUTHORIZED = ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 }
